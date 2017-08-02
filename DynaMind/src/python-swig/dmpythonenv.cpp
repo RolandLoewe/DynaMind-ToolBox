39dcaee010cd2dcb8fe368da720cd54cf6318a9f
rolo <rolo@env.dtu.dk>
2017-08-02 19:20:49 +0200

    Ensure correct SQLITE links
    -moved sqlite into main dir and modified cmake lists to make sure that
    the self-compiled sqlite-dll is actually used by all programs (otherwise
    QT5 SQL plugin will have a different compiler version than sqlite3)
    -created a first hook for a new installation file with Qt5
    -some fixes in swig/pythonenv to ensure py3 compatibility

--- DynaMind/src/python-swig/dmpythonenv.cpp
+++ DynaMind/src/python-swig/dmpythonenv.cpp
@@ -117,6 +117,7 @@ bool PythonEnv::addOverWriteStdCout() {
 	addPythonPath("/usr/bin");
 	addPythonPath("/usr/local/lib");
 	addPythonPath("/usr/lib");
+    addPythonPath("c:/Users/rolo/Documents/GitlabGBAR/gdal_spatiallite/DM2.0/DynaMind-ToolBox/DIST");
 
 	ostringstream script;
 	script << "from osgeo import ogr\n";
@@ -158,15 +159,15 @@ bool PythonEnv::addOverWriteStdCout() {
 	script << "if not isinstance(sys.stdout,Logger):\n";
 	script << "        sys.stdout=Logger(sys.stdout,False)\n";
 	script << "        sys.stderr=Logger(sys.stderr,True)\n";
-	script << "print \"Redirect python stdout and stderr\"\n";
-	//script << "print sys.path\n";
-	/*script << "import PyQt4\n";
+	script << "print (\"Redirect python stdout and stderr\")\n";
+	//script << "print (sys.path)\n";
+	/*script << "import (PyQt4)\n";
 	script << "import reimport\n";
 	script << "import netCDF4\n";
 	script << "import osgeo\n";
 	script << "import numpy\n";
 	script << "import scipy\n";*/
-	script << "print \"Python environment is ready for use\"\n";
+	script << "print (\"Python environment is ready for use\")\n";
 
 	SWIG_PYTHON_THREAD_BEGIN_BLOCK;
 	PyRun_String(script.str().c_str(), Py_file_input, priv->main_namespace, 0);
